72194be0b71917b989455361d7efb9426e934aeb
stzSangJunPark <sangjun.park@sundaytoz.com>
2018-04-17 13:49:07 +0900

    fff

---
+++ TimeLine/Assets/RightControl/RightControlBehaviour.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[Serializable]
+public class RightControlBehaviour : PlayableBehaviour
+{
+    public Color color;
+    public float range;
+    public float intensity;
+}
