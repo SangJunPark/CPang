f1c4f8e6fa58236eab2fd8d34cf3128460a6bea9
stzSangJunPark <sangjun.park@sundaytoz.com>
2018-04-18 19:20:25 +0900

    ddd

--- DK/Assets/Script/TimeLineManager.cs
+++ DK/Assets/Script/TimeLineManager.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
-public class TimeLineManager : MonoBehaviour{
-
-    public PlayableDirector director;
+public class TimeLineManager : MonoBehaviour
+{
+    PlayableDirector director;
+    public List<TimelineAsset> timelines;
 
     static TimeLineManager _instance;
 
-    public TimeLineManager()
-    {
-    }
-
     public static TimeLineManager GetInstance()
     {
-        if (_instance == null)
-        {
-            _instance = new TimeLineManager();
-        }
         return _instance;
     }
 
     public void LoadAsset()
+    {
+        var loaded = Resources.Load("TimeLines/SceneTL") as TimelineAsset;
+        if(loaded)
+            timelines.Add(loaded);
+        director.playableAsset = loaded;
+        director.RebuildGraph();
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        director = this.GetComponent<PlayableDirector>();
+
+        _instance.LoadAsset();
+        _instance.Play();
+    }
+
+    public void Play()
+    {
+        director.Play();
+    }
+
+    public void Preload()
     {
 
     }
